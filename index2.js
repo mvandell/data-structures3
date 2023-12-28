9c8d10b2205ded2f6586b3c7ef7c1743c597a504
Marisa <marisavandellos@gmail.com>
2023-12-27 16:01:50 -0800

    started part 2

--- index2.js
+++ index2.js
@@ -1,1 +1,16 @@
 //PART 2 Breadth First Search
+const graph = {
+    A: ["B", "C"],
+    B: ['A', 'D', 'E'],
+    C: ['A', 'F'],   
+    D: ['B'],   
+    E: ['B', 'F'],   
+    F: ['C', 'E']
+}
+
+//how to find endpoint
+function bfs(graph, startV){
+    const visited = new Set();
+    const queue = [startV];
+    visited.add(startV)
+}
